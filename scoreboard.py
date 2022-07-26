aff72f18bf6dca41a1caf6cc1c22206a5f97d6bd
Omar Ortiz <53305878+oortizmcp@users.noreply.github.com>
2022-07-26 11:22:46 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -21,14 +21,7 @@ class Scoreboard(Turtle):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
-
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
-
-
-
-
-
-
